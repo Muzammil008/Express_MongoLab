ea65c08ad448dd613373b935100971f31dad92b3
Muzammil <muzammilahmed973@gmail.com>
2018-04-23 14:13:04 +0500

    upload express project on github

---
+++ main.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var MongoClient = require('mongodb').MongoClient
+var bodyParser = require('body-parser')
+var app = express();
+var db;
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(function(req, res, next){
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+    next();
+})
+MongoClient.connect('url', (err, client) => {
+    if (err) return console.log(err)
+    db = client.db('abc') // whatever your database name is
+   
+  })
+app.get('/', function (req, res) {
+    db.collection('User').find().toArray(function(err, results) {
+        console.log(results)
+        res.send(results)
+        // send HTML file populated with quotes here
+      })
+})
+app.listen(3000, function () {
+    console.log('listening on 3000')
+})
